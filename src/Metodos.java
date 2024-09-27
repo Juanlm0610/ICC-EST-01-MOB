1e7254d6c78ca451a24bc0679232d950b52c6fbb
Juan Pablo Leon Montenegro <juanpablo.leonm15@gmail.com>
2024-09-27 11:23:04 -0500

    COm

--- src/Metodos.java
+++ src/Metodos.java
@@ -1,13 +1,13 @@
 public class Metodos {
 
-    public int[] sortByBubble(int[] arreglo){
+    public int[] sortByBubble(int[] arreglo) {
 
         int tam = arreglo.length;
-        
-        for (int i = 0 ; i < tam ; i++){
-            for(int j = i+1 ; j < tam ; j++){
 
-                if (arreglo[i] > arreglo[j]){
+        for (int i = 0; i < tam; i++) {
+            for (int j = i + 1; j < tam; j++) {
+
+                if (arreglo[i] > arreglo[j]) {
                     int aux = arreglo[i];
                     arreglo[i] = arreglo[j];
                     arreglo[j] = aux;
@@ -17,21 +17,21 @@ public class Metodos {
         }
         return arreglo;
     }
-    public void prntArreglo(int[] arreglo){
-        System.out.print("Menor a mayor = " );
-        for (int elemento : arreglo){
 
-            System.out.print(" "+ elemento +" ");
+    public void prntArreglo(int[] arreglo) {
+        System.out.print("Menor a mayor = ");
+        for (int elemento : arreglo) {
+
+            System.out.print(" " + elemento + " ");
 
         }
     }
 
+    public int[] sortByBubbleDesc(int[] arreglo) {
+        for (int i = 0; i < arreglo.length; i++) {
+            for (int j = i + 1; j < arreglo.length; j++) {
 
-    public int[] sortByBubbleDesc(int[] arreglo){
-        for (int i = 0 ; i < arreglo.length ; i++){
-            for(int j = i+1 ; j < arreglo.length ; j++){
-
-                if (arreglo[i] < arreglo[j]){
+                if (arreglo[i] < arreglo[j]) {
                     int aux = arreglo[i];
                     arreglo[i] = arreglo[j];
                     arreglo[j] = aux;
@@ -42,25 +42,25 @@ public class Metodos {
         int arreglodesc[] = arreglo;
 
         return arreglodesc;
-    }    
-    
-    public void prntArreglodesc(int[] arreglodesc){
-        System.out.print("Mayor a menor = " );
-        for (int elemento : arreglodesc){
+    }
 
-            System.out.print(" "+ elemento +" ");
+    public void prntArreglodesc(int[] arreglodesc) {
+        System.out.print("Mayor a menor = ");
+        for (int elemento : arreglodesc) {
+
+            System.out.print(" " + elemento + " ");
 
         }
 
     }
 
-    public String[] sortByBubblePalabras(String[] palabras){
+    public String[] sortByBubblePalabras(String[] palabras) {
 
         int tam = palabras.length;
-        
-        for (int i = 0 ; i < tam ; i++){
-            for(int j = i+1 ; j < tam ; j++){
-                if (palabras[i].compareToIgnoreCase(palabras[j]) > 0){
+
+        for (int i = 0; i < tam; i++) {
+            for (int j = i + 1; j < tam; j++) {
+                if (palabras[i].compareToIgnoreCase(palabras[j]) > 0) {
                     String aux = palabras[i];
                     palabras[i] = palabras[j];
                     palabras[j] = aux;
@@ -70,16 +70,14 @@ public class Metodos {
         }
         return palabras;
     }
-    public void prntArregloP(String[] palabras){
-        System.out.print("Orden Alfabetico = " );
-        for (String elemento : palabras){
 
-            System.out.print(" "+ elemento +" ");
+    public void prntArregloP(String[] palabras) {
+        System.out.print("Orden Alfabetico = ");
+        for (String elemento : palabras) {
+
+            System.out.print(" " + elemento + " ");
 
         }
     }
 
-
 }
-
-
